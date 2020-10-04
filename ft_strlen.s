66c22f10850229526975bf029dc54c21f1b868c4
Henry Buisseret <henrybuisseret@gmail.com>
2020-10-04 16:57:07 +0200

    comment ft_strlen

--- ft_strlen.s
+++ ft_strlen.s
@@ -1,15 +1,15 @@
 	        global _ft_strlen
 
 _ft_strlen:
-	        xor		rax, rax
-	        cmp		rdi, 0
-	        je		_return
+	        xor		rax, rax					;mettre a 0, rax = valeur de retour
+	        cmp		rdi, 0						;cmp = campare, rdi premier parametre (rsi 2eme, rdx, ...)
+	        je		_return						;je = jump equal, si compare est egal
 
 _while:
-	        cmp		byte [rdi + rax], 0
+	        cmp		byte [rdi + rax], 0			;rdi[rax] ? 0 (char *rdi)
 	        je		_return
 	        inc		rax
 	        jmp		_while
 
 _return:
-	ret
+			ret									; return rax
