ab87899ca133f0b742836c55e2979f5536c38428
Henry Buisseret <henrybuisseret@gmail.com>
2020-10-07 19:24:49 +0200

    strdup

---
+++ ft_strdup.s
@@ -0,0 +1,28 @@
+        global      _ft_strdup
+        extern      _ft_strlen
+        extern      _ft_strcpy
+        extern      _malloc
+
+_ft_strdup:
+        xor         rax, rax
+        xor         rsi, rsi
+        cmp         rdi, 0
+        je          return
+
+_while:
+        push        rdi
+        call        _ft_strlen
+        mov         rdi, rax
+        inc         rdi
+
+        call        _malloc
+        cmp         rax, 0
+        je          _return
+        pop         rsi
+        mov         rdi, rax
+        call        _ft_strcpy
+        ret
+
+_return:
+        mov         rax, -1
+        ret
